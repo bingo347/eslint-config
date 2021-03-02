ba51bafd4c09bbdd7d822f2c00ae9972d9dee77b
Dmitriy Belyaev <mail@d-belyaev.ru>
2021-03-03 02:24:33 +0300

    feat(eslint-base): add to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+import ESLintBaseBestPracticesRules from './eslint-base/best-practices-rules';
+import ESLintBaseECMAScript6Rules from './eslint-base/ecma-script6-rules';
+import ESLintBasePossibleErrorsRules from './eslint-base/possible-errors-rules';
+import ESLintBaseStrictModeRules from './eslint-base/strict-mode-rules';
+import ESLintBaseStylisticIssuesRules from './eslint-base/stylistic-issues-rules';
+import ESLintBaseVariablesRules from './eslint-base/variables-rules';
+import ESLintBaseExtendedFromTypescriptPluginRules from './eslint-base/extended-from-typescript-plugin-rules';
+
 export default {
-    rules: {}
+    rules: {
+        ...ESLintBaseBestPracticesRules,
+        ...ESLintBaseECMAScript6Rules,
+        ...ESLintBasePossibleErrorsRules,
+        ...ESLintBaseStrictModeRules,
+        ...ESLintBaseStylisticIssuesRules,
+        ...ESLintBaseVariablesRules,
+        ...ESLintBaseExtendedFromTypescriptPluginRules
+    }
 };
