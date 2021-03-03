1d41912715cdef02f7e511814f02d053c68fee21
Dmitriy Belyaev <mail@d-belyaev.ru>
2021-03-04 00:38:21 +0300

    feat(eslint-comments): add rules types

---
+++ src/eslint-comments/types.ts
@@ -0,0 +1,31 @@
+import type { Linter } from 'eslint';
+import type { PrefixedRuleSet } from '../utils';
+
+type CommentTypes =
+    | 'eslint'
+    | 'eslint-disable'
+    | 'eslint-disable-line'
+    | 'eslint-disable-next-line'
+    | 'eslint-enable'
+    | 'eslint-env'
+    | 'exported'
+    | 'global'
+    | 'globals';
+
+export type ESLintCommentsRules = PrefixedRuleSet<'eslint-comments', {
+    'disable-enable-pair': Linter.RuleEntry<[{
+        allowWholeFile: boolean;
+    }]>;
+    'no-aggregating-enable': Linter.RuleEntry;
+    'no-duplicate-disable': Linter.RuleEntry;
+    'no-unlimited-disable': Linter.RuleEntry;
+    'no-unused-disable': Linter.RuleEntry;
+    'no-unused-enable': Linter.RuleEntry;
+    'no-restricted-disable': Linter.RuleEntry<string[]>;
+    'no-use': Linter.RuleEntry<[{
+        allow: CommentTypes[]
+    }]>;
+    'require-description': Linter.RuleEntry<[{
+        ignore: CommentTypes[]
+    }]>;
+}>;
