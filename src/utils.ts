e25fda8a06da3956d63688f6d73253fffb148fcb
Dmitriy Belyaev <mail@d-belyaev.ru>
2021-03-04 00:53:49 +0300

    feat(utils): add RuleWithConfig

--- src/utils.ts
+++ src/utils.ts
@@ -4,4 +4,4 @@ export type PrefixedRuleSet<P extends string, R extends Linter.RulesRecord> = {
     [K in keyof R as `${P}/${string & K}`]: R[K];
 };
 
-export const makePrefix = <P extends string>(prefix: P) => <R extends string>(rule: R) => `${prefix}/${rule}` as const;
+export type RuleWithConfig<R extends Record<string, unknown>> = Linter.RuleEntry<[Partial<R>]>;
