1700fc5f21967ccb9a6f8b0a074821eed6f1542a
Dmitriy Belyaev <mail@d-belyaev.ru>
2021-03-04 22:50:13 +0300

    feat(utils): add enum for rule level

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,11 @@
 import type { Linter } from 'eslint';
 
+export const enum L {
+    Off   = 0,
+    Warn  = 1,
+    Error = 2,
+}
+
 export type PrefixedRuleSet<P extends string, R extends Linter.RulesRecord> = {
     [K in keyof R as `${P}/${string & K}`]: R[K];
 };
