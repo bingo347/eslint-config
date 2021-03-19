c0b65c4a2fb41ce4ec49f05abc70df07980e785c
Dmitriy Belyaev <mail@d-belyaev.ru>
2021-03-19 13:54:22 +0300

    feat(eslint-base): update variable rules

--- src/eslint-base/variables-rules.ts
+++ src/eslint-base/variables-rules.ts
@@ -1,11 +1,12 @@
 import type {VariablesRules} from './types';
+import {L} from '../utils';
 
 const rules: VariablesRules = {
-    'no-delete-var': ['off'],
-    'no-label-var': ['off'],
-    'no-undef': ['off'],
-    'no-undef-init': ['off'],
-    'no-undefined': ['off'],
+    'no-delete-var': L.Error,
+    'no-undef-init': L.Error,
+    'no-undefined':  L.Error,
+    'no-undef':      L.Off, // typescript check it better
+    'no-label-var':  L.Off,
 };
 
 export default rules;
