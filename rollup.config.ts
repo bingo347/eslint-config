a40dc8b1e5f8310571b4a6f041fbc097dc9c978c
Dmitry Belyaev <mail@d-belyaev.ru>
2021-06-22 15:53:53 +0300

    chore: upd build

---
+++ rollup.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="node" />
+import * as path from 'path';
+import tsPlugin from 'rollup-plugin-ts';
+import {terser} from 'rollup-plugin-terser';
+
+export default {
+    input:  path.join(__dirname, 'src/index.ts'),
+    output: {
+        file:    path.join(__dirname, 'index.js'),
+        format:  'cjs',
+        exports: 'default',
+        plugins: [
+            terser(),
+        ],
+    },
+    plugins: [
+        tsPlugin(),
+    ],
+};
