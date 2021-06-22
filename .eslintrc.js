810bc02d3c010d4b9188310a0a991f11adb447da
Dmitry Belyaev <mail@d-belyaev.ru>
2021-06-22 15:54:37 +0300

    chore: upd eslint config for self checking

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    parser: '@typescript-eslint/parser',
     parserOptions: {
         project: [
             path.resolve(__dirname, './tsconfig.json')
         ],
         tsconfigRootDir: __dirname
     },
-    plugins: [
-        '@typescript-eslint',
-        'eslint-comments'
-    ],
     extends: [
         require.resolve('./index.js')
     ],
