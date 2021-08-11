797d29c93d17be235272412b67464437ecd605b6
Dmitry Belyaev <mail@d-belyaev.ru>
2021-08-11 22:27:59 +0300

    feat(import): add to config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import ESLintCommentsRules from './eslint-comments/rules';
 import ESLintBaseBestPracticesRules from './eslint-base/best-practices-rules';
 import ESLintBaseECMAScript6Rules from './eslint-base/ecma-script6-rules';
+import ESLintBaseExtendedFromTypescriptPluginRules from './eslint-base/extended-from-typescript-plugin-rules';
 import ESLintBasePossibleErrorsRules from './eslint-base/possible-errors-rules';
+import ESLintBaseRestrictedRules from './eslint-base/restricted-rules';
 import ESLintBaseStrictModeRules from './eslint-base/strict-mode-rules';
 import ESLintBaseStylisticIssuesRules from './eslint-base/stylistic-issues-rules';
 import ESLintBaseVariablesRules from './eslint-base/variables-rules';
-import ESLintBaseExtendedFromTypescriptPluginRules from './eslint-base/extended-from-typescript-plugin-rules';
-import ESLintBaseRestrictedRules from './eslint-base/restricted-rules';
+import ESLintCommentsRules from './eslint-comments/rules';
+import ImportRules from './import/rules';
 import typescriptBaseRules from './typescript/base-rules';
 import TypescriptExtRules from './typescript/extended-rules';
 
@@ -15,6 +16,13 @@ export default {
     plugins: [
         '@typescript-eslint',
         'eslint-comments',
+        'import',
+    ],
+    settings: {
+        'import/core-modules': ['electron', 'vscode'],
+    },
+    extends: [
+        'plugin:import/typescript',
     ],
     rules: {
         ...ESLintCommentsRules,
@@ -28,5 +36,6 @@ export default {
         ...ESLintBaseRestrictedRules,
         ...typescriptBaseRules,
         ...TypescriptExtRules,
+        ...ImportRules,
     },
 };
