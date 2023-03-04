9078d7c816b2e61c984b5a8ebf111d17e6bf7820
Dmitry Belyaev <mail@d-belyaev.ru>
2023-03-04 08:11:03 +0300

    chore: update dependencies

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,6 +3,8 @@ import * as path from 'path';
 import {terser} from 'rollup-plugin-terser';
 import tsPlugin from 'rollup-plugin-ts';
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 export default {
     input:  path.join(__dirname, 'src/index.ts'),
     output: {
